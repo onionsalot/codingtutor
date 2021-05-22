2c9238d7dc1cfd7371a94d7b49c281c8c92a0f2a
alexandermak <alexandermrak23@gmail.com>
2021-05-22 08:09:33 -0500

    Creates student model and superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Student
 
-class Student():
+# class Student():
 
-    def __init__(self, name, email, password, zipcode):
-        self.name = name 
-        self.email = email 
-        self.password = password
-        self.zipcode = zipcode
+#     def __init__(self, name, email, password, zipcode):
+#         self.name = name 
+#         self.email = email 
+#         self.password = password
+#         self.zipcode = zipcode
     
-students = [
-    Student('Bob', 'bob@gmail.com', 'abc123', '90932'),
-    Student('Jerry', 'jerry@gmail.com', 'abcd1234', '74302'),
-]
+# students = [
+#     Student('Bob', 'bob@gmail.com', 'abc123', '90932'),
+#     Student('Jerry', 'jerry@gmail.com', 'abcd1234', '74302'),
+# ]
 
 # Create your views here.
 def home(request):
+    students = Student.objects.all()
     return render(request, 'home.html', {'students': students})
