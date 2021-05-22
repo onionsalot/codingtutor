c879077cd85d9a6271134ae21ea8001f8b14a518
alexandermak <alexandermrak23@gmail.com>
2021-05-22 07:43:44 -0500

    Adds fake data

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Student():
+
+    def __init__(self, name, email, password, zipcode):
+        self.name = name 
+        self.email = email 
+        self.password = password
+        self.zipcode = zipcode
+    
+students = [
+    Student('Bob', 'bob@gmail.com', 'abc123', '90932'),
+    Student('Jerry', 'jerry@gmail.com', 'abcd1234', '74302'),
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'students': students})
