2a6eb3e1bbe159ae331e4392f66a5e1bd780168e
alexandermak <alexandermrak23@gmail.com>
2021-05-23 17:22:00 -0500

    gets authentication working

--- codingtutor/settings.py
+++ codingtutor/settings.py
@@ -123,4 +123,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+CORS_ORIGIN_WHITELIST=(
+    'https://localhost:3000',
+)
+
+JWT_AUTH={
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'main_app.utils.jwt_response_handler',
+}
+
 CORS_ALLOW_ALL_ORIGINS=True
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
-from rest_framework import serializers
+from django.http import HttpResponseRedirect
+from rest_framework import serializers, permissions, status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.shortcuts import render
 from .models import Student
 from django.views.generic.edit import CreateView
-from .serializers import StudentSerializer 
+from .serializers import StudentSerializer, UserSerializer, UserSerializerWithToken 
 
 
 # Create your views here.
@@ -20,3 +21,19 @@ class StudentCreate(CreateView):
     model = Student
     fields = '__all__'
     
+@api_view(['GET'])
+def current_user(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+class UserList(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
