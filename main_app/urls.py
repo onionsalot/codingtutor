268909774a64b2e873f06a502f08afd54f62fc74
Trong Nguyen <superonion@Trongs-Air.fios-router.home>
2021-05-26 13:53:14 -0400

    Adds django-js-choices

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .views import current_user, UserList, all_profiles, add_slot
+from django_js_choices.views import choices_js
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('jschoices/', choices_js, name='js_choices'),
     path('users/', UserList.as_view()),
     path('current_user/', current_user),
     path('all_profiles/', all_profiles),
