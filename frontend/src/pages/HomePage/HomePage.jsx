5fd675b5dad32e24ce1840d31baff9d1bb60256e
Trong Nguyen <superonion@Trongs-Air.fios-router.home>
2021-06-11 17:11:25 -0400

    Adds useRef to force both user AND tutor to load before geocode is run

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 import TutorList from "../../components/TutorList/TutorList";
@@ -8,7 +8,8 @@ import "./HomePage.css";
 export default function HomePage({ user }) {
   const [tutors, setTutors] = useState([]);
   const [tutorDisplay, setTutorDisplay] = useState([]);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
+  const previousVals = useRef({ tutors, user }); // useRef for making sure tutors AND users have both loaded
   useEffect(() => {
     async function fetchData() {
       const data = await axios
@@ -19,7 +20,9 @@ export default function HomePage({ user }) {
         })
         .then((response) => {
           // alteredResponse takes the response and only gathers the ones that have a rate AKA is an instructor
-          const alteredResponse = response.data.filter(res => res.rate !== null)
+          const alteredResponse = response.data.filter(
+            (res) => res.rate !== null
+          );
           setTutors(alteredResponse);
         });
     }
@@ -28,47 +31,59 @@ export default function HomePage({ user }) {
 
   useEffect(() => {
     async function googleDistance() {
-      const placeIds = [];
-      tutors.forEach((tutor) => {
-        placeIds.push(`place_id:${tutor.place_id}|`);
-      });
-      const proxyurl = "https://fierce-wildwood-46381.herokuapp.com/";
-      const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=place_id:ChIJowv49br0wokRBi5L14DyqTo&destinations=${placeIds.join('')}&key=${process.env.REACT_APP_GOOGLE_KEY}`;
+      if (
+         // useRef for making sure tutors AND users have both loaded
+        previousVals.current.tutors !== tutors &&
+        Object.keys(user).length !== 0
+      ) {
+        console.log("Geocode run");
+        const placeIds = [];
+        tutors.forEach((tutor) => {
+          placeIds.push(`place_id:${tutor.place_id}|`);
+        });
+        const proxyurl = "https://fierce-wildwood-46381.herokuapp.com/";
+        const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=place_id:ChIJowv49br0wokRBi5L14DyqTo&destinations=${placeIds.join(
+          ""
+        )}&key=${process.env.REACT_APP_GOOGLE_KEY}`;
 
-      try{
-        await fetch(proxyurl + url)
-        .then(response => response.json())
-        .then(async (contents) => {
-          if (contents.status === "OK") {
-            const distances = contents.rows[0].elements
-            const unorderedTutors = []
-            tutors.forEach((tutor, idx) => {
-              if(user.id !== tutor.user) {
-                unorderedTutors.push(<TutorList key={idx} tutor={tutor} distance={distances[idx]}/>)  
+        try {
+          await fetch(proxyurl + url)
+            .then((response) => response.json())
+            .then(async (contents) => {
+              if (contents.status === "OK") {
+                const distances = contents.rows[0].elements;
+                const unorderedTutors = [];
+                tutors.forEach((tutor, idx) => {
+                  if (user.id !== tutor.user) {
+                    unorderedTutors.push(
+                      <TutorList
+                        key={idx}
+                        tutor={tutor}
+                        distance={distances[idx]}
+                      />
+                    );
+                  }
+                });
+                unorderedTutors.sort(function (a, b) {
+                  return (
+                    a.props.distance.distance.value -
+                    b.props.distance.distance.value
+                  );
+                });
+                setTutorDisplay(unorderedTutors);
               }
-            })
-            unorderedTutors.sort(function(a, b) { 
-              return a.props.distance.distance.value - b.props.distance.distance.value;
             });
-            setTutorDisplay(unorderedTutors)
-
-          } 
-          // else {
-          //   console.log(contents.status)
-          //   console.log('else triggered')
-          //   setError("There seems to be an issue with google's location matrix... Distance features have been disabled temporarily. We apologize for the inconvenience")
-          //   setTutorDisplay(tutors.map((tutor, idx) => <TutorList key={idx} userId={user.id} tutor={tutor} distance={0}/>))
-          // }
+        } catch {
+          console.log("catch triggered");
+          setError(
+            "There seems to be an issue with google's location matrix... Distance features have been disabled temporarily. We apologize for the inconvenience"
+          );
         }
-        )
-      } catch {
-        console.log('catch triggered')
-        setError("There seems to be an issue with google's location matrix... Distance features have been disabled temporarily. We apologize for the inconvenience")
+        previousVals.current = { tutors, user };
       }
     }
     googleDistance();
-  }, [tutors, user.id]);
-  
+  }, [tutors, user]);
 
   return (
     <>
