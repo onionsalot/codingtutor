a7e07211359d9c38d83463cac3b02138ff60ca15
Trong Nguyen <superonion@Trongs-MacBook-Air.local>
2021-06-13 20:04:17 -0400

    Fixes for reviews not posting. Removes layout guides for details

--- frontend/src/components/TutorDetail/TutorDetailsReviews.jsx
+++ frontend/src/components/TutorDetail/TutorDetailsReviews.jsx
@@ -57,36 +57,37 @@ export default function TutorDetailsReviews({ userId, tutorId }) {
 
   return (
     <div>
+      <h1>Reviews</h1>
       <div className="reviewCards">
       {newReviews}
       </div>
-      <h1>Reviews</h1>
+      <h1>Add Review</h1>
       <form onSubmit={handleSubmit}>
         <p> Rating: </p>
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1" onChange={handleChange} />
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio1" value="1" onChange={handleChange} />
           <label class="form-check-label" for="inlineRadio1">1</label>
         </div>
 
 
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2" onChange={handleChange} />
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio2" value="2" onChange={handleChange} />
           <label class="form-check-label" for="inlineRadio2">2</label>
         </div>
 
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="3" onChange={handleChange} />
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio3" value="3" onChange={handleChange} />
           <label class="form-check-label" for="inlineRadio3">3</label>
         </div>
 
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="4" onChange={handleChange} />
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio4" value="4" onChange={handleChange} />
           <label class="form-check-label" for="inlineRadio4">4</label>
         </div>
 
 
         <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="5" onChange={handleChange} />
+          <input class="form-check-input" type="radio" name="rating" id="inlineRadio5" value="5" onChange={handleChange} />
           <label class="form-check-label" for="inlineRadio5">5</label>
         </div>
 
