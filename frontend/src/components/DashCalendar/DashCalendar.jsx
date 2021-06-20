3d03e1b65cc7f3471658ba1df3ec06e0ca765bcf
Trong Nguyen <superonion@Trongs-MacBook-Air.local>
2021-06-19 21:52:41 -0400

    Adds multiselect to time slots

--- frontend/src/components/DashCalendar/DashCalendar.jsx
+++ frontend/src/components/DashCalendar/DashCalendar.jsx
@@ -6,29 +6,43 @@ import React, { useState, useEffect } from "react";
 
 export default function DashCalendar({ user }) {
   const [value, onChange] = useState(new Date());
+  const [form, setForm] = useState({
+    date: value.toLocaleDateString("en-US"),
+  });
+  const [slotsArray, setSlotsArray] = useState([])
 
   function onClickDay(value, event) {
     const newDate = value.toLocaleDateString("en-US");
+    setForm({ ...form, date: newDate });
   }
 
   function onCalChange(value, event) {
     onChange(value);
     const newDate = value.toLocaleDateString("en-US");
+
   }
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log(e.data)
+    console.log(form)
+    console.log(slotsArray)
   }
 
-  function handleChange(e) {}
+  function handleChange(e) {
+    if (e.target.checked === true) {
+      slotsArray.push(e.target.value)
+    } else {
+      setSlotsArray(slotsArray.filter(i => i !== e.target.value))
+    }
+  }
+
   //   const tileContent = ({ date, view }) => view === 'month' && slots.find(e => e['date'] === date.toLocaleDateString("en-US")) ? "*" : null;
 
   const timeChoices = []
   for (let i = 8; i < 21; i++) {
     timeChoices.push(<div id="ck-button">
       <label>
-        <input type="checkbox" value={i<10? `0${i}`: `${i}`} />
+        <input type="checkbox" onChange={handleChange} value={i<10? `0${i}`: `${i}`} />
         <span>{i<13? `${i} am`:`${i-12} pm`}</span>
       </label>
     </div>)
@@ -47,34 +61,6 @@ export default function DashCalendar({ user }) {
         <div className="dashChoices">
           <label><h2>Select A time</h2></label><br/>
           {timeChoices}
-          <select name="hour" onChange={handleChange}>
-            <option value="00" selected>
-              12:00 AM
-            </option>
-            <option value="01">1:00 AM</option>
-            <option value="02">2:00 AM</option>
-            <option value="03">3:00 AM</option>
-            <option value="04">4:00 AM</option>
-            <option value="05">5:00 AM</option>
-            <option value="06">6:00 AM</option>
-            <option value="07">7:00 AM</option>
-            <option value="08">8:00 AM</option>
-            <option value="09">9:00 AM</option>
-            <option value="10">10:00 AM</option>
-            <option value="11">11:00 AM</option>
-            <option value="12">12:00 PM</option>
-            <option value="13">1:00 PM</option>
-            <option value="14">2:00 PM</option>
-            <option value="15">3:00 PM</option>
-            <option value="16">4:00 PM</option>
-            <option value="17">5:00 PM</option>
-            <option value="18">6:00 PM</option>
-            <option value="19">7:00 PM</option>
-            <option value="20">8:00 PM</option>
-            <option value="21">9:00 PM</option>
-            <option value="22">10:00 PM</option>
-            <option value="23">11:00 PM</option>
-          </select>
           <br />
           <Button type="submit"> CONFIRM TIME SLOT </Button>
         </div>
